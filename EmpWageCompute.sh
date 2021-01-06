ddbb3da902e66c8d66468fdcaf3adf92e5f7b421
Suraj-92 <suraj.temkar.92@gmail.com>
2021-01-06 19:27:48 +0530

    Usercase -2 done

--- EmpWageCompute.sh
+++ EmpWageCompute.sh
@@ -1,1 +1,12 @@
+#!/bin/bash -x
 echo "Welcome to the branch"
+
+isPresent=1
+checkAttendance=$((RANDOM%2))
+
+if [ $checkAttendance -eq $isPresent ]
+then 
+	echo "Employee is present"
+else
+	echo "Employee is absent"
+fi
