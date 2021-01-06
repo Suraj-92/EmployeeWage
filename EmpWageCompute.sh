d44073d5a6231d9dbec035896be45b2fa8de0c2a
Suraj-92 <suraj.temkar.92@gmail.com>
2021-01-06 20:04:20 +0530

    DailyEmpWage

--- EmpWageCompute.sh
+++ EmpWageCompute.sh
@@ -2,11 +2,16 @@
 echo "Welcome to the branch"
 
 isPresent=1
+wagePerHrs=20
+noOfHrs=8
 checkAttendance=$((RANDOM%2))
 
 if [ $checkAttendance -eq $isPresent ]
 then 
 	echo "Employee is present"
+	payment=$(($wagePerHrs*$noOfHrs))
+	echo "$payment"
 else
 	echo "Employee is absent"
+	echo "Payment is 0"
 fi
